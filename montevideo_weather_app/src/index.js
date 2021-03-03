645e330157d126383069b7e9b0907ddce11c6c85
RodoAgus <rodoagussm@gmail.com>
2021-03-03 00:21:47 -0300

    Se importa el archivo css al main para que cargue con las propiedad dadas a los elementos.

--- montevideo_weather_app/src/index.js
+++ montevideo_weather_app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
+import './Weather.css';
 import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
