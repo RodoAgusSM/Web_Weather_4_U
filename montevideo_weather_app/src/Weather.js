e838605f42dede6eea0bb84d02e7c16ad71c531e
RodoAgus <rodoagussm@gmail.com>
2021-03-03 23:56:32 -0300

    Se agrega un spinner a mano para los tiempos de carga, mostrar que la aplicacion esta cargando al usuario.

--- montevideo_weather_app/src/Weather.js
+++ montevideo_weather_app/src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import logo from './imgs/sun-half.png';
+import loading from './imgs/loading.gif';
 
 function Weather() {
     let [cityName, setCityName] = useState([]);
@@ -14,9 +15,10 @@ function Weather() {
     let [windSpeed, setWindSpeed] = useState([]);
     let [windDirection, setWindDirection] = useState([]);
     let [date, setDate] = useState([]);
+    let [isLoading, setIsLoading] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Montevideo&units=Metric&lang=sp&APPID=" + process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+            let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Montevideo&units=Metric&lang=sp&APPID=" + process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
             const response = await axios.get(weatherUrl);
             setCityName(response.data.name);
             setCountryNameShort(response.data.sys.country);
@@ -31,33 +33,40 @@ function Weather() {
             let directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSO", "SO", "OSO", "O", "ONO", "NO", "NNO"]
             let cardinal = parseInt((degrees + 11.25) / 22.5);
             setWindDirection(directions[cardinal % 16]);
-            var date = new Date();
-            var time = date.getHours() + ":" + date.getMinutes() + " " + date.getDay() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+            let date = new Date();
+            let time = date.getHours() + ":" + date.getMinutes() + " " + date.getDay() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
             setDate(time);
+            setIsLoading(false);
         };
+        setIsLoading(true);
         fetchData();
         setInterval(() => {
             fetchData();
         }, 120000);
     }, []);
+    let toShow;
+    if (isLoading) toShow = <div><img id="spinner" src={loading} alt="Loading" />;</div>
+    else toShow = <div className="weatherDiv" id="weatherCard">
+        <img id="logoApp" src={logo} alt="Logo" />
+        <div id="titleApp">
+            <h2>Tiempo actual en {cityName} ({countryNameShort})</h2>
+        </div>
+        <img id="weatherIcon" src={"https://openweathermap.org/img/w/" + icon + ".png"} alt="Icon" />
+        <div id="weatherMain">
+            <code id="weatherMainTemp">{realFeel}°C</code><br />
+            <code id="description">{description}</code><br />
+            <code>Sensación térmica: {feelsLike}°C</code><br />
+            <code>{date}</code>
+        </div>
+        <div id="weatherData">
+            <code>Humedad: {humidity}%</code><br />
+            <code>Presión: {pressure} hPa</code><br />
+            <code>Viento: {windDirection} {windSpeed} km/h</code><br />
+        </div>
+    </div>;
     return (
-        <div className="weatherDiv" id="weatherCard">
-            <img id="logoApp" src={logo} alt="Logo" />
-            <div id="titleApp">
-                <h2>Tiempo actual en {cityName} ({countryNameShort})</h2>
-            </div>
-            <img id="weatherIcon" src={"https://openweathermap.org/img/w/" + icon + ".png"} alt="Icon" />
-            <div id="weatherMain">
-                <code id="weatherMainTemp">{realFeel}°C</code><br />
-                <code id="description">{description}</code><br />
-                <code>Sensación térmica: {feelsLike}°C</code><br />
-                <code>{date}</code>
-            </div>
-            <div id="weatherData">
-                <code>Humedad: {humidity}%</code><br />
-                <code>Presión: {pressure} hPa</code><br />
-                <code>Viento: {windDirection} {windSpeed} km/h</code><br />
-            </div>
+        <div>
+            {toShow}
         </div>
     );
 }
