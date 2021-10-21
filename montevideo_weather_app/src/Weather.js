c2d3cee1afc2b47b9c2478b5a824e1819681a056
RodoAgus <rodoagussm@gmail.com>
2021-10-20 23:04:49 -0300

    Cambio en tiempo del spinner.

--- montevideo_weather_app/src/Weather.js
+++ montevideo_weather_app/src/Weather.js
@@ -76,7 +76,7 @@ function Weather() {
 					let iconUrl = iconURL + iconValue.current + iconExtension;
 					let iconFetched = await axios.get(iconUrl);
 					setIcon(iconFetched?.config?.url);
-				}, 1500);
+				}, 1200);
 			} catch (error) {
 				setIsIconWorking(false);
 			}
