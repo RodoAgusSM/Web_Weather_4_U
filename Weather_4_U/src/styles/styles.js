a46f95bb781d011ed294b47d3b97caa9cc66f2eb
RodoAgus <rodoagussm@gmail.com>
2022-08-19 00:26:05 -0300

    - Making the app responsive for mobile devices for the air pollution new feature.

--- Weather_4_U/src/styles/styles.js
+++ Weather_4_U/src/styles/styles.js
@@ -4,10 +4,10 @@ import { Colors } from './colors';
 let desktopWidth = 540 + 'px';
 let desktopHeight = 577 + 'px';
 let mobileWidth = window.innerWidth * 0.85 + 'px';
-let mobileHeightSmallDisplay = '508px'; //Done
-let mobileHeightBigDisplay = '580px'; //Done
-let mobileHeight12and13ProMax = '580px'; //Done
-let mobileHeight12and13RegularAndPro = '576px'; //Done
+let mobileHeightSmallDisplay = '508px'; //To Do
+let mobileHeightBigDisplay = '580px'; //To Do
+let mobileHeight12and13ProMax = '580px'; //To Do
+let mobileHeight12and13RegularAndPro = '576px'; //To Do
 let mobileHeight11ProMax = '580px'; //Done
 let mobileHeight11ProAnd13Mini = '520px'; //Done
 let mobileHeight11Regular = '580px'; //Done
@@ -51,9 +51,18 @@ export const WeatherIcon = styled.img.attrs((props) => ({
 		margin-top: 6px;
 		margin-left: 16px;
 	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		margin-top: 2px;
+		margin-left: 5px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		margin-top: 2px;
+		margin-left: -3px;
+		width: 92px;
+	}
 	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
-		margin-top: 8px;
-		margin-left: 16px;
+		margin-top: 6px;
+		margin-left: 8px;
 	}
 	width: 102px;
 `;
@@ -224,6 +233,17 @@ export const WeatherMain = styled.div`
 		margin-top: -118px;
 		margin-left: 132px;
 	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		margin-left: 130px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		margin-top: -105px;
+		margin-left: 132px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		margin-top: -110px;
+		margin-left: 132px;
+	}
 `;
 
 export const WeatherMainTemperature = styled.code`
@@ -235,6 +255,15 @@ export const WeatherMainTemperature = styled.code`
 	}
 	@media only screen and (max-width: 375px) {
 		font-size: 21px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		margin-left: -6px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		margin-left: -30px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		margin-left: -4px;
 	}
 `;
 
@@ -248,6 +277,16 @@ export const Code = styled.code`
 	@media only screen and (min-width: 376px) and (max-width: 712px) {
 		font-size: 12px;
 	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		font-size: 11px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		margin-left: -30px;
+		font-size: 11px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		font-size: 11px;
+	}
 	font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
 `;
 
@@ -257,6 +296,9 @@ export const WeatherData = styled.div`
 	font-size: 16px;
 	cursor: default;
 	@media (max-width: 768px) {
+		margin-left: 132px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
 		margin-left: 132px;
 	}
 `;
@@ -504,9 +546,33 @@ export const MoreInfoButton = styled.span`
 	@media only screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) {
 		font-size: 11px;
 	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		font-size: 11px;
+	}
 	font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
 	&:hover {
 		color: ${Colors.yellowLemon};
+	}
+`;
+
+export const AirPollutionItemLegendIcon = styled.span`
+	width: 30px;
+	height: 30px;
+	background-color: ${(props) => props.color};
+	border-radius: 25px;
+`;
+
+export const AirPollutionLegendText = styled.code`
+	margin-left: 4px;
+	margin-right: 12px;
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		font-size: 10px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		font-size: 10px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		font-size: 10px;
 	}
 `;
 
@@ -514,6 +580,15 @@ export const AirPollutionItemContainer = styled.div`
 	margin-top: 40px;
 	display: flex;
 	align-items: center;
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		font-size: 13px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		font-size: 13px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		font-size: 13px;
+	}
 `;
 
 export const AirPollutionItemSpan = styled.span`
@@ -522,4 +597,16 @@ export const AirPollutionItemSpan = styled.span`
 	background-color: ${(props) => props.color};
 	border-radius: 25px;
 	margin-left: 10px;
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) {
+		width: 25px;
+		height: 25px;
+	}
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		width: 25px;
+		height: 25px;
+	}
+	@media only screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) {
+		width: 25px;
+		height: 25px;
+	}
 `;
